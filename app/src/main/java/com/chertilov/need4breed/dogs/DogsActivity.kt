b066213f79e0bfb2ae1518649adbab402ffb860d
Vladimir Chertilov <chertilov@reksoft.ru>
2021-04-13 13:19:01 +0300

    added dogs activity

---
+++ app/src/main/java/com/chertilov/need4breed/dogs/DogsActivity.kt
@@ -0,0 +1,13 @@
+package com.chertilov.need4breed.dogs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chertilov.need4breed.R
+
+class DogsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dogs)
+    }
+}
